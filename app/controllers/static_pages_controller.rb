229fec75776837c9ce2d5101082d86e98e67e332
junpei ono <snorlax.chemist.and.jazz@gmail.com>
2018-12-09 00:49:30 +0900

    不要なインサートを削除

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,16 +1,5 @@
 class StaticPagesController < ApplicationController
   def home
-
-    product = Product.new
-    product.attributes = {barcode_id: 234, name: "kabifon", price:432}
-    product.save
-
-    puts '@@@'
-    puts Product.first
-    puts '@@@'
-
-
-
     #render html: "hello, world!"
     render :json => Product.all
   end
