44a0aa48d8b83d8d777804268a3dd5e816053de2
junpei ono <snorlax.chemist.and.jazz@gmail.com>
2019-02-03 15:57:56 +0900

    02/04 Add is_public colum to Product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_03_053449) do
+ActiveRecord::Schema.define(version: 2019_02_03_065355) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2019_02_03_053449) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_url"
+    t.boolean "is_public"
   end
 
   create_table "users", force: :cascade do |t|
